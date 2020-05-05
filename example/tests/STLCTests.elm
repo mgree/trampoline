56f8764f8014b7181d2da8e061f62948c215a94a
Michael Greenberg <michael.greenberg@pomona.edu>
2020-05-04 22:59:49 -0700

    parseExpr tests

--- example/tests/STLCTests.elm
+++ example/tests/STLCTests.elm
@@ -10,29 +10,42 @@ import Random
 import Parser exposing (Parser)
 import Parser
 
+expectParse : Parser a -> String -> a -> Expect.Expectation
+expectParse p s expected =
+    case (Parser.run p s) of
+        Err err -> Expect.fail ("expected successful parse of " ++ s)
+        Ok v -> Expect.equal v expected
+
+    
 parsesAs : Parser a -> String -> a -> Test
 parsesAs p s expected =
-    test s (\() -> case (Parser.run p s) of
-                       Err err -> Expect.fail ("expected successful parse of " ++ s)
-                       Ok v -> Expect.equal v expected)
+    test s (\() -> expectParse p s expected)
 
 suite : Test
 suite =
     describe "STLC"
         [ describe "parseType"
-              [ parsesAs parseType "int" TInt
-              , parsesAs parseType "(int)" TInt
-              , parsesAs parseType "int->int" (TFun TInt TInt)
-              , parsesAs parseType " int    ->\n\rint" (TFun TInt TInt)
-              , parsesAs parseType "(int->int)" (TFun TInt TInt)
-              , parsesAs parseType "(int->int)->int" (TFun (TFun TInt TInt) TInt)
-              , parsesAs parseType "int->int->int" (TFun TInt (TFun TInt TInt))
-              , parsesAs parseType "int->(int->int)" (TFun TInt (TFun TInt TInt))
-              , parsesAs parseType "((int->(int->int)))" (TFun TInt (TFun TInt TInt))
-              , parsesAs parseType "int->(int->int)->int" (TFun TInt (TFun (TFun TInt TInt) TInt))
-              ]
-{-        , describe "parseExpr" []
-        , describe "typeOf" []
+            [ parsesAs parseType "int" TInt
+            , parsesAs parseType "(int)" TInt
+            , parsesAs parseType "int->int" (TFun TInt TInt)
+            , parsesAs parseType " int    ->\n\rint" (TFun TInt TInt)
+            , parsesAs parseType "(int->int)" (TFun TInt TInt)
+            , parsesAs parseType "(int->int)->int" (TFun (TFun TInt TInt) TInt)
+            , parsesAs parseType "int->int->int" (TFun TInt (TFun TInt TInt))
+            , parsesAs parseType "int->(int->int)" (TFun TInt (TFun TInt TInt))
+            , parsesAs parseType "((int->(int->int)))" (TFun TInt (TFun TInt TInt))
+            , parsesAs parseType "int->(int->int)->int" (TFun TInt (TFun (TFun TInt TInt) TInt))
+            ]
+        , describe "parseExpr"
+            [ parsesAs parseExpr "x" (EVar "x")
+            , parsesAs parseExpr "xYZ_Q12" (EVar "xYZ_Q12")
+            , parsesAs parseExpr "  xYZ_Q13" (EVar "xYZ_Q13")
+            , fuzz (Fuzz.intRange 0 1024) "all numbers parse"
+                (\n -> expectParse parseExpr (String.fromInt n) (ENum n))
+            , parsesAs parseExpr "1 2 3 4 5" (EApp (EApp (EApp (EApp (ENum 1) (ENum 2)) (ENum 3)) (ENum 4)) (ENum 5))
+            , parsesAs parseExpr "\\x:int. x" (ELam "x" TInt (EVar "x"))
+            ]
+{-        , describe "typeOf" []
         , describe "eval" []
         , describe "evalCEK" [] -}
         ]
